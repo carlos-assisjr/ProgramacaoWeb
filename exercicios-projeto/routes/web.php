18ff8b956fb8b5f0f19ac68672f6f1c0654827b0
José Carlos <carlos.assis.jr@hotmail.com>
2026-04-03 01:17:12 -0300

    saves

--- exercicios-projeto/routes/web.php
+++ exercicios-projeto/routes/web.php
@@ -5,3 +5,28 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+use App\Http\Controllers\CategoriaController;
+
+Route::get('/paginainicial', function () {
+    return view('welcome');
+});
+
+Route::get('/exercicio', function() {
+    return view('exercicio');
+});
+
+Route::post('/resposta', function(Request $request) {
+    $valor1 = $request->input('valor1');
+    $valor2 = $request->input('valor2');
+    $soma = $valor1 + $valor2;
+    return("A soma é: $soma");
+});
+
+Route::resource('categorias', CategoriaController::class);
+
+Route::resource('produtos', ProdutoController::class);
